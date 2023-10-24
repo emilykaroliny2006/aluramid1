ebcf7454b3d2018f6567bef8626f2abfd8015fd5
emilykaroliny2006 <129808025+emilykaroliny2006@users.noreply.github.com>
2023-10-23 21:27:44 -0300

    emilylinda

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ function somTocaGrilo (){
 document.querySelector("#som_tecla_grilo").play();
 }
 document.querySelector(".tecla_grilo").onclick =somTocaGrilo;
+const listaDeTeclas = document.querySelectorAll(".tecla");
+listaDeTeclas[4].onclik = tocaSomGrilo;
